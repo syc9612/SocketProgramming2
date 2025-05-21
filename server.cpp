f44b32911201ed79b112f995b23a4f15926db51e
unknown <syc9612@ad.somansa.com>
2025-05-21 10:07:08 +0900

    create: first commit

---
+++ server.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <memory>
+#include <boost/bind/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/asio.hpp>
+#include <boost/array.hpp>
+
+int main(){
+    std::cout << "test" << std::endl;
+}
